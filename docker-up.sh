b232b523af96f7c68e7e89bb9ff8b993ecfcb904
inaseem <inaseem@protonmail.com>
2024-10-24 15:14:54 +0530

    feat: docker

---
+++ docker-up.sh
@@ -0,0 +1,9 @@
+docker-compose down 
+if [ "$1" = "--build" ]
+then 
+    docker-compose up -d --build
+else 
+    docker-compose up -d
+fi
+
+sh ssh-to-app.sh 
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,29 @@
-// src/index.ts
 import express, { Express, Request, Response } from 'express';
+import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
+const database = process.env.POSTGRES_DB!;
+const db_user = process.env.POSTGRES_USER!;
+const db_password = process.env.POSTGRES_PASSWORD!;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+const sequelize = new Sequelize(database, db_user, db_password, {
+  host: 'postgres',
+  dialect: 'postgres',
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+(async () => {
+  await sequelize.authenticate();
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+})().catch((err) => {
+  console.error('Unable to connect to the database:', err);
+  sequelize.close();
 });
